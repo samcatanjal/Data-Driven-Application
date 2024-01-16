407056ec0fee087aa1ca6df98131ec8c1fd6949b
samcatanjal <scatanjal@gmail.com>
2024-01-16 11:10:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,200 +1,253 @@
-from tkinter import *
+import tkinter as tk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-class api_handling:
-    def get_data(self, num):
-        response = requests.get('https://api.potterdb.com/v1/books')
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        data = response.json()['data'][num]['attributes']
-        title = data['title']
-        cover = data['cover']
-        bg_image = self.load_image(cover)
-        return bg_image
-
-
-    def load_image(self, cover):
-        cover_response = requests.get(cover)
-        image = Image.open(BytesIO(cover_response.content))
-        image = image.resize((300, 350)) 
-        bg_image = ImageTk.PhotoImage(image)
-        return bg_image
-
-api = api_handling()
-
-class BasePage(Frame):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        self.place(width=600, height=650, relx=0.5, rely=0.5, anchor=CENTER)
-
-class BaseFrame(Frame):
-    def __init__(self, parent, bg_image=None):
-        super().__init__(parent)
-
-        self.place(width=300, height=350, relx=0.5, rely=0.45, anchor=CENTER)
-
-        if bg_image:
-            self.bg_image = bg_image
-            self.background_label = Label(self, image=bg_image)
-            self.background_label.place(relwidth=1, relheight=1)
-
-class FirstPage(BasePage):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        bg_image = api.get_data(0) # retrieving image data
-        if bg_image:
-            Label(self, image=bg_image).place(relx=0.5, rely=0.5, anchor=CENTER)
-
-        button = Button(self, text="Next", width=10, font=("Montserrat", 12), command=lambda: parent.show_page(parent.second))
-        button.place(relx=0.9, rely=0.1, anchor=CENTER)
-
-        self.main = BaseFrame(self, bg_image)
-
-        view = Button(self, text="Select", width=30, font=("Montserrat", 12))
-        view.place(relx=0.5, rely=0.85, anchor=CENTER)
-
-class SecondPage(BasePage):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        bg_image = api.get_data(1) # retrieving image data
-        if bg_image:
-            Label(self, image=bg_image).place(relx=0.5, rely=0.5, anchor=CENTER)
-
-        button = Button(self, text="Back", width=10, font=("Montserrat", 12), command=lambda: parent.show_page(parent.first))
-        button.place(relx=0.1, rely=0.1, anchor=CENTER)
-
-        button = Button(self, text="Next", width=10, font=("Montserrat", 12), command=lambda: parent.show_page(parent.third))
-        button.place(relx=0.9, rely=0.1, anchor=CENTER)
-
-        self.main = BaseFrame(self, bg_image)
-
-        view = Button(self, text="Select", width=30, font=("Montserrat", 12))
-        view.place(relx=0.5, rely=0.85, anchor=CENTER)
-
-class ThirdPage(BasePage):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        bg_image = api.get_data(2) # retrieving image data
-        if bg_image:
-            Label(self, image=bg_image).place(relx=0.5, rely=0.5, anchor=CENTER)
-
-        button = Button(self, text="Back", width=10, font=("Montserrat", 12), command=lambda: parent.show_page(parent.second))
-        button.place(relx=0.1, rely=0.1, anchor=CENTER)
-
-        button = Button(self, text="Next", width=10, font=("Montserrat", 12), command=lambda: parent.show_page(parent.fourth))
-        button.place(relx=0.9, rely=0.1, anchor=CENTER)
-
-        self.main = BaseFrame(self, bg_image)
-
-        view = Button(self, text="Select", width=30, font=("Montserrat", 12))
-        view.place(relx=0.5, rely=0.85, anchor=CENTER)
-
-class FourthPage(BasePage):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        bg_image = api.get_data(3) # retrieving image data
-        if bg_image:
-            Label(self, image=bg_image).place(relx=0.5, rely=0.5, anchor=CENTER)
-
-
-        button = Button(self, text="Back", width=10, font=("Montserrat", 12), command=lambda: parent.show_page(parent.third))
-        button.place(relx=0.1, rely=0.1, anchor=CENTER)
-
-        button = Button(self, text="Next", width=10, font=("Montserrat", 12), command=lambda: parent.show_page(parent.fifth))
-        button.place(relx=0.9, rely=0.1, anchor=CENTER)
-
-        self.main = BaseFrame(self, bg_image)
-
-        view = Button(self, text="Select", width=30, font=("Montserrat", 12))
-        view.place(relx=0.5, rely=0.85, anchor=CENTER)
-
-class FifthPage(BasePage):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        bg_image = api.get_data(4) # retrieving image data
-        if bg_image:
-            Label(self, image=bg_image).place(relx=0.5, rely=0.5, anchor=CENTER)
-
-        button = Button(self, text="Back", width=10, font=("Montserrat", 12), command=lambda: parent.show_page(parent.fourth))
-        button.place(relx=0.1, rely=0.1, anchor=CENTER)
-
-        button = Button(self, text="Next", width=10, font=("Montserrat", 12), command=lambda: parent.show_page(parent.sixth))
-        button.place(relx=0.9, rely=0.1, anchor=CENTER)
-
-        self.main = BaseFrame(self, bg_image)
-
-        view = Button(self, text="Select", width=30, font=("Montserrat", 12))
-        view.place(relx=0.5, rely=0.85, anchor=CENTER)
-
-class SixthPage(BasePage):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        bg_image = api.get_data(5) # retrieving image data
-        if bg_image:
-            Label(self, image=bg_image).place(relx=0.5, rely=0.5, anchor=CENTER)
-
-        button = Button(self, text="Back", width=10, font=("Montserrat", 12), command=lambda: parent.show_page(parent.fifth))
-        button.place(relx=0.1, rely=0.1, anchor=CENTER)
-
-        button = Button(self, text="Next", width=10, font=("Montserrat", 12), command=lambda: parent.show_page(parent.seventh))
-        button.place(relx=0.9, rely=0.1, anchor=CENTER)
-
-        self.main = BaseFrame(self, bg_image)
-
-        view = Button(self, text="Select", width=30, font=("Montserrat", 12))
-        view.place(relx=0.5, rely=0.85, anchor=CENTER)
-
-class SeventhPage(BasePage):
-    def __init__(self, parent):
-        super().__init__(parent)
-
-        bg_image = api.get_data( 6) # retrieving image data
-        if bg_image:
-            Label(self, image=bg_image).place(relx=0.5, rely=0.5, anchor=CENTER)
-
-    
-        button = Button(self, text="Back", width=10, font=("Montserrat", 12), command=lambda: parent.show_page(parent.sixth))
-        button.place(relx=0.1, rely=0.1, anchor=CENTER)
-
-        self.main = BaseFrame(self, bg_image)
-
-        view = Button(self, text="Select", width=30, font=("Montserrat", 12))
-        view.place(relx=0.5, rely=0.85, anchor=CENTER)
-
-
-class App(Tk):
+class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
         self.title('Harry Potter Books')
         self.geometry('700x750')
         self.resizable(0,0)
-        self['bg'] = '#212121'
 
-        self.first = FirstPage(self)
-        self.second = SecondPage(self)
-        self.third = ThirdPage(self)
-        self.fourth = FourthPage(self)
-        self.fifth = FifthPage(self)
-        self.sixth = SixthPage(self)
-        self.seventh = SeventhPage(self)
+        self.one = FirstPage(self) # putting all pages in app
+        self.two = SecondPage(self)
+        self.three = ThirdPage(self)
+        self.four = FourthPage(self)
+        self.five= FifthPage(self)
+        self.six = SixthPage(self)
+        self.seven = SeventhPage(self)
 
-        self.show_page(self.first)
+        self.show_page(self.one) # initially shows first page
 
-        self.mainloop()
-
-    def raise_frame(self, frame):
+    def raise_frame(self, frame): # frame switching using tkraise
         frame.tkraise()
 
-    def show_page(self, page):
+    def show_page(self, page): # indicate which page to raise (for button command purposes)
         self.raise_frame(page)
 
-App()
+class APIHandling:
+    @staticmethod # acts as a function that can be used in other classes (without calling instance)
+    def getData():
+        url = 'https://api.potterdb.com/v1/books' # url of api
+        response = requests.get(url)
+        data = response.json() # reads data as json file
+        return data # saves data
+    
+    @staticmethod
+    def get_image(url):
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
+        return img # saves image
+
+class BasePage(tk.Frame): # template page
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self['bg'] = "#212121"
+        self.place(relx=0.5, rely=0.5, anchor=tk.CENTER, width=700, height=750) # fills whole window
+
+    def display_img(self, img_url): # display image settings
+        img = APIHandling.get_image(img_url)
+        img = img.resize((300, 350))
+        img = ImageTk.PhotoImage(img)
+
+        img_label = tk.Label(self, image=img)
+        img_label.image = img
+        img_label.place(relx=0.5, rely=0.45, anchor=tk.CENTER)
+
+class PopUp(tk.Toplevel): # pop up window
+    def __init__(self, parent, data):
+        super().__init__(parent)
+
+        self.title('Book Details')
+        self.geometry('750x750')
+        self.resizable(0,0)
+        self['bg'] = '#333333'
+
+        self.data = data # gets data from parent window (pages)
+
+        self.display_details()
+    def display_details(self):
+        title = self.data['attributes']['title'] # fetching each info
+        author = self.data['attributes']['author']
+        summary = self.data['attributes']['summary']
+        date = self.data['attributes']['release_date']
+        pages = self.data['attributes']['pages']
+        dedication = self.data['attributes']['dedication']
+
+        title_label = tk.Label(self, text=f'{title}', font=("Montserrat", 16), bg='#333333', fg='#FFFFFF') # puting info into labels
+        author_label = tk.Label(self, text=f'{author}', font=("Montserrat", 12), bg='#333333', fg='#FFFFFF')
+        summary_label = tk.Label(self, text=f'{summary}', font=("Montserrat", 12), bg='#333333', fg='#FFFFFF', wraplength=500)
+        date_label = tk.Label(self, text=f'Release Date: {date}', font=("Montserrat", 12), bg='#333333', fg='#FFFFFF')
+        pages_label = tk.Label(self, text=f'Pages: {pages}', font=("Montserrat", 12), bg='#333333', fg='#FFFFFF')
+        dedication_label = tk.Label(self, text=f'{dedication}', font=("Montserrat", 12), bg='#333333', fg='#FFFFFF', wraplength=600)
+
+        title_label.pack(side='top', anchor='w', padx=20, pady=40) # widget layout
+        author_label.pack(side='bottom', anchor='center', pady=25)
+        summary_label.place(relx=0.5, rely=0.35, anchor=tk.CENTER)
+        date_label.place(relx=0.2, rely=0.8, anchor=tk.CENTER)
+        pages_label.place(relx=0.85, rely=0.8, anchor=tk.CENTER)
+        dedication_label.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
+
+ 
+class FirstPage(BasePage):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.data = APIHandling.getData() # data fetching
+
+        next = tk.Button(self, text="Next", font=('Montserrat', 14), width=10, bg='#333333', fg='#fff', command=lambda: parent.show_page(parent.two))
+        select = tk.Button(self, text="Select", font=('Montserrat', 16), width=20, bg='#333333', fg='#fff', command=lambda: self.showbook(self.data['data'][0]))
+        
+        next.place(relx=0.85, rely=0.1, anchor=tk.CENTER)
+        select.pack(side="bottom", pady=150)
+        
+        self.display() # display image
+
+    def showbook(self, book): # function for opening pop up window
+        pop_up = PopUp(self, book)
+
+    def display(self):
+        cover = self.data['data'][0]['attributes']['cover'] # gets image from api image url
+        self.display_img(cover)
+
+class SecondPage(BasePage):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.data = APIHandling.getData()
+
+        back = tk.Button(self, text="Back", font=('Montserrat', 14), width=10, bg='#333333', fg='#fff', command=lambda: parent.show_page(parent.one))
+        next = tk.Button(self, text="Next", font=('Montserrat', 14), width=10, bg='#333333', fg='#fff', command=lambda: parent.show_page(parent.three))
+        select = tk.Button(self, text="Select", font=('Montserrat', 16), width=20, bg='#333333', fg='#fff', command=lambda: self.showbook(self.data['data'][1]))
+
+        next.place(relx=0.85, rely=0.1, anchor=tk.CENTER)
+        back.place(relx=0.15, rely=0.1, anchor=tk.CENTER)
+        select.pack(side="bottom", pady=150)
+
+        self.display()
+
+    def showbook(self, book):
+        pop_up = PopUp(self, book)
+
+    def display(self):
+        cover = self.data['data'][1]['attributes']['cover']
+        self.display_img(cover)
+
+class ThirdPage(BasePage):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.data = APIHandling.getData()
+
+        back = tk.Button(self, text="Back", font=('Montserrat', 14), width=10, bg='#333333', fg='#fff', command=lambda: parent.show_page(parent.two))
+        next = tk.Button(self, text="Next", font=('Montserrat', 14), width=10, bg='#333333', fg='#fff', command=lambda: parent.show_page(parent.four))
+        select = tk.Button(self, text="Select", font=('Montserrat', 16), width=20, bg='#333333', fg='#fff', command=lambda: self.showbook(self.data['data'][2]))
+
+        next.place(relx=0.85, rely=0.1, anchor=tk.CENTER)
+        back.place(relx=0.15, rely=0.1, anchor=tk.CENTER)
+        select.pack(side="bottom", pady=150)
+
+        self.display()
+
+    def showbook(self, book):
+        pop_up = PopUp(self, book)
+
+    def display(self):
+        cover = self.data['data'][2]['attributes']['cover']
+        self.display_img(cover)
+
+class FourthPage(BasePage):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.data = APIHandling.getData()
+
+        back = tk.Button(self, text="Back", font=('Montserrat', 14), width=10, bg='#333333', fg='#fff', command=lambda: parent.show_page(parent.three))
+        next = tk.Button(self, text="Next", font=('Montserrat', 14), width=10, bg='#333333', fg='#fff', command=lambda: parent.show_page(parent.five))
+        select = tk.Button(self, text="Select", font=('Montserrat', 16), width=20, bg='#333333', fg='#fff', command=lambda: self.showbook(self.data['data'][3]))
+
+        next.place(relx=0.85, rely=0.1, anchor=tk.CENTER)
+        back.place(relx=0.15, rely=0.1, anchor=tk.CENTER)
+        select.pack(side="bottom", pady=150)
+
+        self.display()
+
+    def showbook(self, book):
+        pop_up = PopUp(self, book)
+
+    def display(self):
+        cover = self.data['data'][3]['attributes']['cover']
+        self.display_img(cover)
+
+class FifthPage(BasePage):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.data = APIHandling.getData()
+
+        back = tk.Button(self, text="Back", font=('Montserrat', 14), width=10, bg='#333333', fg='#fff', command=lambda: parent.show_page(parent.four))
+        next = tk.Button(self, text="Next", font=('Montserrat', 14), width=10, bg='#333333', fg='#fff', command=lambda: parent.show_page(parent.six))
+        select = tk.Button(self, text="Select", font=('Montserrat', 16), width=20, bg='#333333', fg='#fff', command=lambda: self.showbook(self.data['data'][4]))
+
+        next.place(relx=0.85, rely=0.1, anchor=tk.CENTER)
+        back.place(relx=0.15, rely=0.1, anchor=tk.CENTER)
+        select.pack(side="bottom", pady=150)
+
+        self.display()
+
+    def showbook(self, book):
+        pop_up = PopUp(self, book)
+
+    def display(self):
+        cover = self.data['data'][4]['attributes']['cover']
+        self.display_img(cover)
+
+class SixthPage(BasePage):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.data = APIHandling.getData()
+
+        back = tk.Button(self, text="Back", font=('Montserrat', 14), width=10, bg='#333333', fg='#fff', command=lambda: parent.show_page(parent.five))
+        next = tk.Button(self, text="Next", font=('Montserrat', 14), width=10, bg='#333333', fg='#fff', command=lambda: parent.show_page(parent.seven))
+        select = tk.Button(self, text="Select", font=('Montserrat', 16), width=20, bg='#333333', fg='#fff', command=lambda: self.showbook(self.data['data'][5]))
+
+        next.place(relx=0.85, rely=0.1, anchor=tk.CENTER)
+        back.place(relx=0.15, rely=0.1, anchor=tk.CENTER)
+        select.pack(side="bottom", pady=150)
+
+        self.display()
+
+    def showbook(self, book):
+        pop_up = PopUp(self, book)
+
+    def display(self):
+        cover = self.data['data'][5]['attributes']['cover']
+        self.display_img(cover)
+
+class SeventhPage(BasePage):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.data = APIHandling.getData()
+
+        back = tk.Button(self, text="Back", font=('Montserrat', 14), width=10, bg='#333333', fg='#fff', command=lambda: parent.show_page(parent.six))
+        select = tk.Button(self, text="Select", font=('Montserrat', 16), width=20, bg='#333333', fg='#fff', command=lambda: self.showbook(self.data['data'][6]))
+
+        back.place(relx=0.15, rely=0.1, anchor=tk.CENTER)
+        select.pack(side="bottom", pady=150)
+
+        self.display()
+
+    def showbook(self, book):
+        pop_up = PopUp(self, book)
+
+    def display(self):
+        cover = self.data['data'][6]['attributes']['cover']
+        self.display_img(cover)
+
+app = App() # making class as an app/window
+app.mainloop() # run
